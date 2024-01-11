9cf48dc8e6d2bc13c111329ac638723e5a6277ed
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2024-01-11 16:20:47 +0900

    Closureを使った実装方法を追加

--- AsyncAwaitTest/AsyncAwaitTest/ViewController.swift
+++ AsyncAwaitTest/AsyncAwaitTest/ViewController.swift
@@ -11,9 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        executeBothTask { total in
+            print("total: \(total)")
+        }
     }
 
-
+    func executeBothTask(completion: @escaping (Int) -> Void) {
+        task1 { [weak self] num1 in
+            self?.task2 { num2 in
+                completion(num1 + num2)
+            }
+        }
+    }
+    
+    private func task1(completion: @escaping (Int) -> Void) {
+        sleep(1)
+        print("タスク1完了")
+        completion(2)
+    }
+    
+    private func task2(completion: @escaping (Int) -> Void) {
+        sleep(2)
+        print("タスク2完了")
+        completion(3)
+    }
 }
 
